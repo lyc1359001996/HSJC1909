6fd3e0e71050c0c10f33ec270a625f4344f9ba66
lyc <1359001996@qq.com>
2020-12-11 16:06:24 +0800

    安装包

--- Nucleic_Acid/View/ReadCardOffline.xaml.cs
+++ Nucleic_Acid/View/ReadCardOffline.xaml.cs
@@ -137,17 +137,33 @@ namespace Nucleic_Acid.View
             CHCUsbSDK.UserID = CHCUsbSDK.USB_SDK_Login(ref StruCurUsbLoginInfo, ref StruDeviceRegRes);
             if (CHCUsbSDK.UserID == CHCUsbSDK.INVALID_USER_ID)
             {
+                changeConn(false);
                 Console.WriteLine(CHCUsbSDK.UserID);
                 CHCUsbSDK.UserID = UserIDTemp;//为了解决重复登录时的问题，但是这次只考虑了只能登录一个设备，两个设备同时能登录的话，ID会覆盖得继续解决ID的问题
             }
             else
             {
+                changeConn(true);
                 string SuccesfulLoginInfo = "The device whose serial number is " + StruCurUsbLoginInfo.szSerialNumber + "login in successfully";
                 Console.WriteLine(SuccesfulLoginInfo);
             }
         }
 
-
+        private void changeConn(bool TF)
+        {
+            if (TF)
+            {
+                conn.Visibility = Visibility.Visible;
+                disconn.Visibility = Visibility.Hidden;
+                connect_Text.Text = "已连接";
+            }
+            else
+            {
+                conn.Visibility = Visibility.Hidden;
+                disconn.Visibility = Visibility.Visible;
+                connect_Text.Text = "未连接";
+            }
+        }
 
         /// <summary>
         /// 判断登录设备的相关信息是否是有效的
